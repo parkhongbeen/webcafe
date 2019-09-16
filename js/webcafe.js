6d95b140ed074256a9c5503b78fe69e6a2f16e97
parkhongbeen <pack122@naver.com>
2019-09-16 19:34:01 +0900

    공지사항,자료실

---
+++ js/webcafe.js
@@ -0,0 +1,9 @@
+var section = $('.board section');
+var tab = $('.tab');
+
+
+tab.on('click', function(e){
+  e.preventDefault();
+    section.removeClass('tab-act');
+    $(this).parent().parent().addClass('tab-act');
+  })
